04bf8c11da48cade0a1cce82d59d498b2ed9faf5
Mario Ramírez <marioh.ramirez@udea.edu.co>
2025-05-17 13:51:00 -0500

    Exercise 1.3 added

--- courseinfo/src/App.jsx
+++ courseinfo/src/App.jsx
@@ -10,7 +10,7 @@ const Part = (props) => {
   return (
     <>
       <p>
-        {props.partDetails.name} {props.partDetails.exercise}
+        {props.partDetails.name} {props.partDetails.exercises}
       </p>
     </>
   );
@@ -36,10 +36,22 @@ const Total = (props) => {
 
 const App = () => {
   const course = "Half Stack application development";
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
   const parts = [
-    { name: "Fundamentals of React", exercise: 10 },
-    { name: "Using props to pass data", exercise: 7 },
-    { name: "State of a component", exercise: 14 },
+    part1,
+    part2,
+    part3
   ];
 
   return (
@@ -48,7 +60,7 @@ const App = () => {
       <Content courseParts={parts} />
       <Total
         numberExercises={
-          parts[0].exercise + parts[1].exercise + parts[2].exercise
+          part1.exercises + part2.exercises + part3.exercises
         }
       />
     </div>
