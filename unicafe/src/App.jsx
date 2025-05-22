4d0aa5d954888b24583ef91ebcabb6b2a14f40e2
Mario Ramírez <marioh.ramirez@udea.edu.co>
2025-05-21 19:24:18 -0500

    Exercise 1.7 added

--- unicafe/src/App.jsx
+++ unicafe/src/App.jsx
@@ -5,42 +5,74 @@ const Button = ({ onClick, text }) => {
 };
 
 const DisplayStats = (props) => {
-  return(
-      <p>{props.text}: {props.value}</p>
-  )
-}
+  if (props.text === "Positive") {
+    return (
+      <p>
+        {props.text}: {props.value} %
+      </p>
+    );
+  }
+  return (
+    <p>
+      {props.text}: {props.value}
+    </p>
+  );
+};
 
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
+  const [total, setTotal] = useState(0);
+  const [average, setAverage] = useState(0);
+  const [positive, setPositive] = useState(0);
 
   const handleGoodClick = () => {
-    setGood(good + 1);
+    const updatedGood = good + 1;
+    setGood(updatedGood);
+    const updatedTotal = updatedGood + bad + neutral;
+    setTotal(updatedTotal);
+    setAverage((updatedGood - bad) / updatedTotal);
+    setPositive((updatedGood * 100) / updatedTotal);
   };
 
   const handleNeutralClick = () => {
-    setNeutral(neutral + 1);
+    const updatedNeutral = neutral + 1;
+    setNeutral(updatedNeutral);
+    const updatedTotal = updatedNeutral + good + bad;
+    setTotal(updatedTotal);
+    setAverage((good - bad) / updatedTotal);
+    setPositive((good * 100) / updatedTotal);
   };
   const handleBadClick = () => {
-    setBad(bad + 1);
+    const updatedBad = bad + 1;
+    setBad(updatedBad);
+    const updatedTotal = updatedBad + good + neutral;
+    setTotal(updatedTotal);
+    setAverage((good - updatedBad) / updatedTotal);
+    setPositive((good * 100) / updatedTotal);
   };
 
-  return <div>
-    <h1>Give Feedback</h1>
+  return (
     <div>
-      <Button onClick={handleGoodClick} text="Good"/>
-      <Button onClick={handleNeutralClick} text="Neutral"/>
-      <Button onClick={handleBadClick} text="Bad"/>
+      <h1>Give Feedback</h1>
+      <div>
+        <Button onClick={handleGoodClick} text="Good" />
+        <Button onClick={handleNeutralClick} text="Neutral" />
+        <Button onClick={handleBadClick} text="Bad" />
+      </div>
+      <div>
+        <h1>Statistics</h1>
+        <DisplayStats text="Good" value={good} />
+        <DisplayStats text="Neutral" value={neutral} />
+        <DisplayStats text="Bad" value={bad} />
+        <DisplayStats text="All" value={total} />
+        <DisplayStats text="Average" value={average} />
+        <DisplayStats text="Positive" value={positive} />
+      </div>
     </div>
-    <div>
-      <h1>Statistics</h1>
-      <DisplayStats text="Good" value={good}/>
-      <DisplayStats text="Neutral" value={neutral}/>
-      <DisplayStats text="Bad" value={bad}/>
-    </div>
-  </div>;
+  );
 };
 
 export default App;
